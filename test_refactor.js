72e19091b3b77cb458b2198357ce335b853557a1
Naomi Owens <ndunhourowens@Naomis-MacBook-Pro.local>
2015-09-23 11:53:14 -1000

    deleted some excessive console logs

--- test_refactor.js
+++ test_refactor.js
@@ -1,8 +1,8 @@
 
 
-function squareRootProb(number) {
+function sumOfSquare(number) {
   var arrayOfNumbers = [];
-  var startingNumber = 1;
+  var startingNumber = 0;
   //make an array of numbers 1 - number
   for(var i = 0; i < number; i++) {
     startingNumber ++;
@@ -18,7 +18,7 @@ var squareRoot = arrayOfNumbers.map(function(current,index,array) {
 
 console.log(squareRoot);
 
-
+//8.38233234744
 
 }
-squareRootProb(5);
+sumOfSquare(5);
